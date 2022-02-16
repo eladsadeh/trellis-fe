4ad283dd1bc6e516d2679bed8f50c35d5d20b6cb
eladsadeh <elad.sadeh1@.gmail.com>
2022-02-15 22:49:33 -0500

    work on varieties and varietyForm

--- components/VarietyForm.js
+++ components/VarietyForm.js
@@ -1,11 +1,166 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from '../styles/varietyform.module.css';
+import utilStyles from '../styles/utils.module.css';
 
-function VarietyForm({variety}) {
-    return (
-        <div>
-            {variety.name}
-        </div>
-    );
+function VarietyForm(props) {
+	const [variety, setVariety] = useState(props.variety);
+	// form data starts with variety changes saved after SAVE
+	const isNewVariety = variety.name ? false : true;
+	const [formData, setFormData] = useState(props.variety);
+	const [edit, setEdit] = useState(isNewVariety ? true : false);
+	const [showDetail, setShowDetail] = useState(isNewVariety ? true : false);
+	const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+	const handleFormChange = (ev) => {
+		setFormData({ ...formData, [ev.target.name]: ev.target.value });
+	};
+
+	const handleSaveOrEdit = async (ev) => {
+		ev.preventDefault();
+		console.log('handle save and edit');
+		// IF not in edit mode, turn on edit
+		if (!edit) {
+			setEdit(!edit);
+		} else {
+			console.log('save:', formData);
+			console.log('new = ', isNewVariety);
+			const method = isNewVariety ? 'POST' : 'PUT';
+			const endPoint = isNewVariety ? '' : `${formData.id}`;
+			const status = isNewVariety ? 201 : 200;
+			const token = localStorage.getItem('token');
+			console.log(`${method}`, `${baseUrl}/varieties/${endPoint}`);
+			console.log(`${method}`, JSON.stringify(formData));
+			if (token) {
+				try {
+					const res = await fetch(`${baseUrl}/varieties/${endPoint}`, {
+						method: `${method}`,
+						headers: {
+							'Content-Type': 'application/json',
+							Authorization: `Token ${token}`,
+						},
+						body: JSON.stringify(formData),
+					});
+					if (res.status === status) {
+						const data = await res.json();
+						console.log('res :', data);
+					} else {
+						console.log('bad res:', res);
+					}
+				} catch (err) {
+					console.log(err);
+				}
+			} else {
+				console.log('no token found');
+				router.push('/login');
+			}
+		}
+	};
+
+	return (
+		<div className={styles.container}>
+			{!showDetail && (
+				<p className={styles.title} onClick={() => setShowDetail(!showDetail)}>
+					{formData.name}
+				</p>
+			)}
+			{showDetail && (
+				<form className='variety-form'>
+					<label className={styles.group}>
+						<span className={styles.label}> Variety Name:</span>
+						<input
+							className={styles.input}
+							name='name'
+							type='text'
+							placeholder='Variety name'
+							value={formData.name}
+							disabled={!edit}
+							onChange={handleFormChange}
+						/>
+					</label>
+					<label className={styles.group}>
+						<span className={styles.label}>Method:</span>
+						<select
+							className={styles.input}
+							name='method'
+							value={formData.method}
+							disabled={!edit}
+							onChange={handleFormChange}>
+							<option value='TP' onChange={handleFormChange}>
+								Transplant
+							</option>
+							<option value='DS'>Direct Seed</option>
+						</select>
+					</label>
+					<label className={styles.group}>
+						<span className={styles.label}>Days to Maturity:</span>
+						<input
+							className={styles.input}
+							style={{ width: '4em' }}
+							name='dtm'
+							type='number'
+							value={formData.dtm}
+							disabled={!edit}
+							onChange={handleFormChange}
+						/>
+					</label>
+					<label className={styles.group}>
+						<span className={styles.label}>Quantity:</span>
+						<input
+							className={styles.input}
+							style={{ width: '4em' }}
+							name='quantity'
+							type='number'
+							value={formData.quantity}
+							disabled={!edit}
+							onChange={handleFormChange}
+						/>
+					</label>
+					<label className={styles.group}>
+						<span className={styles.label}>Seeds/Oz:</span>
+						<input
+							className={styles.input}
+							style={{ width: '6em' }}
+							name='seeds_oz'
+							type='number'
+							value={formData.seeds_oz}
+							disabled={!edit}
+							onChange={handleFormChange}
+						/>
+					</label>
+					{!isNewVariety && (
+						<button
+							className={utilStyles.button}
+							type='button'
+							onClick={() => {
+								setShowDetail(!showDetail);
+								setEdit(!edit)}}>
+							Close
+						</button>
+					)}
+					<button
+						className={utilStyles.button}
+						type='button'
+						onClick={(ev) => handleSaveOrEdit(ev)}>
+						{edit ? 'Save' : 'Edit'}
+					</button>
+					{!isNewVariety && (
+						<button
+							className={utilStyles.button}
+							onClick={(ev) => ev.preventDefault()}>
+							Delete
+						</button>
+					)}
+					{edit && (
+						<button
+							className={utilStyles.button}
+							onClick={() => setEdit(!edit)}>
+							Cancel
+						</button>
+					)}
+				</form>
+			)}
+		</div>
+	);
 }
 
 export default VarietyForm;
--- pages/varieties.js
+++ pages/varieties.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { Icon } from '@blueprintjs/core';
 import Layout from '../components/Layout';
+import StepLink from '../components/StepLink';
 import VarietyForm from '../components/VarietyForm';
+import styles from '../styles/varieties.module.css';
 
 function Varieties(props) {
 	const [cropsData, setCropsData] = useState(null);
-	const [newVariety, setNewVariety] = useState('');
+	// const [newVariety, setNewVariety] = useState('');
 	const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 	const getCropsData = async () => {
@@ -36,6 +39,23 @@ function Varieties(props) {
 		}
 	};
 
+	const addVariety = (idx, cropId) => {
+		console.log('add variety for:', cropId, cropsData[idx]);
+		const newVariety = {
+			crop_id: cropId,
+			name: '',
+			method: 'DS',
+			dtm: '',
+			seeds_oz: '',
+			quantity: '',
+		};
+		// Make a copy of crops state
+		const crops = [...cropsData];
+		// Push the new variety to the crop
+		crops[idx].varieties.push(newVariety);
+		setCropsData(crops);
+	};
+
 	useEffect(() => {
 		console.log('varieties is mounting:');
 		getCropsData();
@@ -51,20 +71,34 @@ function Varieties(props) {
 
 	return (
 		<Layout>
-			<div>Varieties</div>
-			{cropsData.map((crop, idx) => {
-				return (
-					<div key={crop.id}>
-						<div>
-							{crop.name} {crop.varieties.length}
+			<StepLink
+				to='varieties'
+				number='2'
+				text='Add the varieties you want to grow'
+			/>
+
+			<div className={styles.container}>
+				{cropsData.map((crop, idx) => {
+					return (
+						<div className={styles.crop} key={idx}>
+							<div className={styles.cropsTitle}>
+								<span>{crop.name}</span>
+								<span>
+									<span className={styles.count}>{crop.varieties.length}</span>
+									<button
+										className={styles.button}
+										onClick={() => addVariety(idx, crop.id)}>
+										<Icon color='gray' size={16} icon='add' />
+									</button>
+								</span>
+							</div>
+							{crop.varieties.map((variety,idx) => {
+								return <VarietyForm key={idx} variety={variety} />;
+							})}
 						</div>
-						{crop.varieties.map((variety) => {
-							console.log(variety);
-							return <VarietyForm key={variety.id} variety={variety} />;
-						})}
-					</div>
-				);
-			})}
+					);
+				})}
+			</div>
 		</Layout>
 	);
 }
